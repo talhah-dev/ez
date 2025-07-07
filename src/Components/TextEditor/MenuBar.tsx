d305bdd468c1042173ca6b58d61b776caafcc9d0
Muhammad Talha <talha18513@gmail.com>
2025-07-07 07:27:33 +0500

    blog setup complete

--- src/Components/TextEditor/MenuBar.tsx
+++ src/Components/TextEditor/MenuBar.tsx
@@ -1,104 +1,104 @@
-import React from 'react'
-import { Editor } from "@tiptap/react"
+// import React from 'react'
+// import { Editor } from "@tiptap/react"
 
 
-const MenuBar = ({ editor }: { editor: Editor | null }) => {
-    if (!editor) return null
+// const MenuBar = ({ editor }: { editor: Editor | null }) => {
+//     if (!editor) return null
 
-    const buttonClass = (isActive: boolean) =>
-        `py-1 px-3 rounded border border-zinc-300 hover:bg-gray-100 transition-all duration-500 ${isActive ? 'bg-black text-white' : 'text-gray-700'
-        }`
+//     const buttonClass = (isActive: boolean) =>
+//         `py-1 px-3 rounded border border-zinc-300 hover:bg-gray-100 transition-all duration-500 ${isActive ? 'bg-black text-white' : 'text-gray-700'
+//         }`
 
-    return (
-        <div className="control-group">
-            <div className="flex items-center gap-2 flex-wrap">
-                <button type="button"
-                    onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
-                    className={buttonClass(editor.isActive('heading', { level: 1 }))}
-                >
-                    H1
-                </button>
-                <button type="button"
-                    onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
-                    className={buttonClass(editor.isActive('heading', { level: 2 }))}
-                >
-                    H2
-                </button>
-                <button type="button"
-                    onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
-                    className={buttonClass(editor.isActive('heading', { level: 3 }))}
-                >
-                    H3
-                </button>
-                <button type="button"
-                    onClick={() => editor.chain().focus().setParagraph().run()}
-                    className={buttonClass(editor.isActive('paragraph'))}
-                >
-                    P
-                </button>
-                <button type="button"
-                    onClick={() => editor.chain().focus().toggleBold().run()}
-                    className={buttonClass(editor.isActive('bold'))}
-                >
-                    Bold
-                </button>
-                <button type="button"
-                    onClick={() => editor.chain().focus().toggleItalic().run()}
-                    className={buttonClass(editor.isActive('italic'))}
-                >
-                    Italic
-                </button>
-                <button type="button"
-                    onClick={() => editor.chain().focus().toggleBulletList().run()}
-                    className={buttonClass(editor.isActive('bulletList'))}
-                >
-                    Bullet list
-                </button>
-                <button type="button"
-                    onClick={() => editor.chain().focus().toggleOrderedList().run()}
-                    className={buttonClass(editor.isActive('orderedList'))}
-                >
-                    Ordered list
-                </button>
-                <button type="button"
-                    onClick={() => editor.chain().focus().toggleStrike().run()}
-                    className={buttonClass(editor.isActive('strike'))}
-                >
-                    Strike
-                </button>
-                <button type="button"
-                    onClick={() => editor.chain().focus().toggleCode().run()}
-                    className={buttonClass(editor.isActive('code'))}
-                >
-                    Code
-                </button>
-                <button type="button"
-                    onClick={() => editor.chain().focus().setHorizontalRule().run()}
-                    className={buttonClass(false)}
-                >
-                    Horizontal rule
-                </button>
-                <button type="button"
-                    onClick={() => editor.chain().focus().setHardBreak().run()}
-                    className={buttonClass(false)}
-                >
-                    break
-                </button>
-                <button type="button"
-                    onClick={() => editor.chain().focus().unsetAllMarks().run()}
-                    className={buttonClass(false)}
-                >
-                    Clear marks
-                </button>
-                <button type="button"
-                    onClick={() => editor.chain().focus().clearNodes().run()}
-                    className={buttonClass(false)}
-                >
-                    Clear nodes
-                </button>
-            </div >
-        </div >
-    )
-}
+//     return (
+//         <div className="control-group">
+//             <div className="flex items-center gap-2 flex-wrap">
+//                 <button type="button"
+//                     onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
+//                     className={buttonClass(editor.isActive('heading', { level: 1 }))}
+//                 >
+//                     H1
+//                 </button>
+//                 <button type="button"
+//                     onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+//                     className={buttonClass(editor.isActive('heading', { level: 2 }))}
+//                 >
+//                     H2
+//                 </button>
+//                 <button type="button"
+//                     onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
+//                     className={buttonClass(editor.isActive('heading', { level: 3 }))}
+//                 >
+//                     H3
+//                 </button>
+//                 <button type="button"
+//                     onClick={() => editor.chain().focus().setParagraph().run()}
+//                     className={buttonClass(editor.isActive('paragraph'))}
+//                 >
+//                     P
+//                 </button>
+//                 <button type="button"
+//                     onClick={() => editor.chain().focus().toggleBold().run()}
+//                     className={buttonClass(editor.isActive('bold'))}
+//                 >
+//                     Bold
+//                 </button>
+//                 <button type="button"
+//                     onClick={() => editor.chain().focus().toggleItalic().run()}
+//                     className={buttonClass(editor.isActive('italic'))}
+//                 >
+//                     Italic
+//                 </button>
+//                 <button type="button"
+//                     onClick={() => editor.chain().focus().toggleBulletList().run()}
+//                     className={buttonClass(editor.isActive('bulletList'))}
+//                 >
+//                     Bullet list
+//                 </button>
+//                 <button type="button"
+//                     onClick={() => editor.chain().focus().toggleOrderedList().run()}
+//                     className={buttonClass(editor.isActive('orderedList'))}
+//                 >
+//                     Ordered list
+//                 </button>
+//                 <button type="button"
+//                     onClick={() => editor.chain().focus().toggleStrike().run()}
+//                     className={buttonClass(editor.isActive('strike'))}
+//                 >
+//                     Strike
+//                 </button>
+//                 <button type="button"
+//                     onClick={() => editor.chain().focus().toggleCode().run()}
+//                     className={buttonClass(editor.isActive('code'))}
+//                 >
+//                     Code
+//                 </button>
+//                 <button type="button"
+//                     onClick={() => editor.chain().focus().setHorizontalRule().run()}
+//                     className={buttonClass(false)}
+//                 >
+//                     Horizontal rule
+//                 </button>
+//                 <button type="button"
+//                     onClick={() => editor.chain().focus().setHardBreak().run()}
+//                     className={buttonClass(false)}
+//                 >
+//                     break
+//                 </button>
+//                 <button type="button"
+//                     onClick={() => editor.chain().focus().unsetAllMarks().run()}
+//                     className={buttonClass(false)}
+//                 >
+//                     Clear marks
+//                 </button>
+//                 <button type="button"
+//                     onClick={() => editor.chain().focus().clearNodes().run()}
+//                     className={buttonClass(false)}
+//                 >
+//                     Clear nodes
+//                 </button>
+//             </div >
+//         </div >
+//     )
+// }
 
-export default MenuBar
+// export default MenuBar
--- src/Components/TextEditor/RichTextEditor.tsx
+++ src/Components/TextEditor/RichTextEditor.tsx
@@ -1,62 +1,62 @@
-"use client"
-import { useEditor, EditorContent } from "@tiptap/react"
-import StarterKit from "@tiptap/starter-kit"
-import { useEffect } from "react"
-import MenuBar from "./MenuBar"
-import TextAlign from '@tiptap/extension-text-align'
-import Highlight from '@tiptap/extension-highlight'
+// "use client"
+// import { useEditor, EditorContent } from "@tiptap/react"
+// import StarterKit from "@tiptap/starter-kit"
+// import { useEffect } from "react"
+// import MenuBar from "./MenuBar"
+// import TextAlign from '@tiptap/extension-text-align'
+// import Highlight from '@tiptap/extension-highlight'
 
-export default function RichTextEditor({
-    content,
-    onChange,
-}: {
-    content: string
-    onChange: (html: string) => void
-}) {
-    const editor = useEditor({
-        extensions: [
-            StarterKit.configure({
-                heading: {
-                    levels: [1, 2, 3],
-                },
-                bulletList:{
-                    HTMLAttributes:{
-                        class:"list-disc ml-4"
-                    }
-                },
-                orderedList:{
-                    HTMLAttributes:{
-                        class:"list-decimal ml-4"
-                    }
-                }
-            }),
-            TextAlign.configure({
-                types: ['heading', 'paragraph'], // enable for these node types
-            }),
-            Highlight
-        ],
+// export default function RichTextEditor({
+//     content,
+//     onChange,
+// }: {
+//     content: string
+//     onChange: (html: string) => void
+// }) {
+//     const editor = useEditor({
+//         extensions: [
+//             StarterKit.configure({
+//                 heading: {
+//                     levels: [1, 2, 3],
+//                 },
+//                 bulletList:{
+//                     HTMLAttributes:{
+//                         class:"list-disc ml-4"
+//                     }
+//                 },
+//                 orderedList:{
+//                     HTMLAttributes:{
+//                         class:"list-decimal ml-4"
+//                     }
+//                 }
+//             }),
+//             TextAlign.configure({
+//                 types: ['heading', 'paragraph'], // enable for these node types
+//             }),
+//             Highlight
+//         ],
 
-        content,
-        onUpdate: ({ editor }) => {
-            onChange(editor.getHTML())
-        },
-        editorProps: {
-            attributes: {
-                class: "min-h-80 w-full border mt-4 p-4 border-zinc-300 rounded-lg bg-zinc-100",
-            }
-        }
-    })
+//         content,
+//         onUpdate: ({ editor }) => {
+//             onChange(editor.getHTML())
+//         },
+//         editorProps: {
+//             attributes: {
+//                 class: "min-h-80 w-full border mt-4 p-4 border-zinc-300 rounded-lg bg-zinc-100",
+//             }
+//         }
+//     })
 
-    useEffect(() => {
-        if (editor && content) {
-            editor.commands.setContent(content)
-        }
-    }, [editor])
+//     useEffect(() => {
+//         if (editor && content) {
+//             editor.commands.setContent(content)
+//         }
+//     }, [editor])
 
-    return (
-        <div className="">
-            <MenuBar editor={editor} />
-            <EditorContent editor={editor} />
-        </div>
-    )
-}
+//     return (
+//         <div className="">
+//             <MenuBar editor={editor} />
+//             <EditorContent editor={editor} />
+//         </div>
+//     )
+// }
